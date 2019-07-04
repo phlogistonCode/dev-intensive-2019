af0617ac9df1096900adaf089ebcfb23222c27fc
phlogiston <alexander.popov@migogroup.ru>
2019-07-04 11:47:43 +0400

    complete task

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,6 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.stripHtml() : String {
+fun String.stripHtml(): String {
     var tag = false
     var whitespace = false
 
@@ -11,38 +11,26 @@ fun String.stripHtml() : String {
         }
 
         if (it.isWhitespace() && whitespace) {
-        } else if (it.isWhitespace()){
+        } else if (it.isWhitespace()) {
             if (!tag) sb.append(it)
             whitespace = true
         } else {
             if (!tag) sb.append(it)
             whitespace = false
         }
-import java.lang.StringBuilder
-
-fun String.truncate(num: Int = 16): String {
-    val sb = StringBuilder()
-    var i = 0
-    for (c in this) {
-        i++
-        if (i <= num)
-            sb.append(c)
-    }
-    var text: CharSequence = sb
-    while (sb.last().isWhitespace())
-        text = sb.dropLastWhile { it.isWhitespace() }
-
-    sb = text.toString()
-
-    if (this.length > num)
-        sb.append("...")
 
         if (it == '>') {
             tag = false
-    return text.toString()
-}
         }
-    }
 
+    }
     return sb.toString()
+}
+
+fun String.truncate(num: Int = 16): String {
+    val result = this.trim()
+    return when {
+        result.length <= num -> result
+        else -> "${result.take(num).trimEnd()}..."
+    }
 }
